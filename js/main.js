236074380bfda19e0216446e6bec73a3072c196f
Michael Blix <m.blix@samsung.com>
2020-06-19 00:56:42 +0000

    Load from data file
    Basic support for swapping other products

--- js/main.js
+++ js/main.js
@@ -4,18 +4,50 @@ let $ = document.querySelector.bind(document);
 let addCartBtn = $('#add-cart-btn');
 let cart = $('#cart');
 
-
 let n = 0;
-addCartBtn.addEventListener('click', function(e){
-  cart.classList.toggle('full');
 
-  if (n % 2 !== 1) {
-    cart.textContent = '(1) Cart';
-    addCartBtn.textContent = 'Added ✔';
-  } else {
-    cart.textContent = '(0) Cart';
-    addCartBtn.textContent = 'Add to Cart';
+
+async function init() {
+  addCartBtn.addEventListener('click', function(e){
+    cart.classList.toggle('full');
+
+    if (n % 2 !== 1) {
+      cart.textContent = '(1) Cart';
+      addCartBtn.textContent = 'Added ✔';
+    } else {
+      cart.textContent = '(0) Cart';
+      addCartBtn.textContent = 'Add to Cart';
+    }
+
+    n++;
+  });
+
+  let resp = await fetch('data/products.json');
+  let data = await resp.json();
+  //console.log(data);
+  let product = data.products[0];
+  displayProduct(product);
+}
+
+function displayProduct(product) {
+  $('p.description').textContent = product.description;
+  $('#price-unit').textContent = product.price;
+  $('#brand').textContent = product.brand;
+  $('#product-name').textContent = product.name;
+
+  let specsTable = $('.specs table');
+  for (let i = 0; i < product.specs.length; i++) {
+    let spec = product.specs[i];
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    tr.appendChild(td);
+    td.textContent = spec;
+    specsTable.appendChild(tr);
   }
 
-  n++;
-});
+  let modelViewer = $('model-viewer');
+  modelViewer.setAttribute('src', product.model);
+}
+
+//
+init();
